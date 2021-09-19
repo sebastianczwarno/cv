854f52ead161adf68decc93aed84b8491057598d
Sebastian Czwarno <sebastianczwarno@gmail.com>
2021-09-19 16:01:57 +0200

    Added netcompany experience and refactoring.

--- src/metadata/metadata.js
+++ src/metadata/metadata.js
@@ -9,7 +9,7 @@ module.exports = {
     ['C#', '++++'],
     ['.Net', '+++'],
     ['EFCore', '+++'],
-    ['Java', '++++'],
+    ['Java', '+++'],
     ['Hibernate', '+'],
     ['SpringBoot', '++'],
     ['Maven', '++'],
@@ -24,9 +24,16 @@ module.exports = {
     {
       company: 'Netcompany',
       position: 'Senior specialist',
-      period: 'Oct 2018 - Present',
+      period: 'Oct 2019 - Present',
       badges: ['C#', '.Net', 'NHibernate', 'javascript', 'Angular.js', 'WPF', 'WCF', 'Azure'],
       contents: `Development and maintenance of Danish Civil Courts system minretssag.dk. Platform is available for all citizens.`
+    },
+    {
+      company: 'Netcompany',
+      position: 'Senior specialist',
+      period: 'Jul 2018 - Dec 2018',
+      badges: ['Java', 'jOOQ', 'Docker'],
+      contents: `Development of internal database migration tool.`
     },
     {
       company: 'Netcompany',
@@ -39,21 +46,21 @@ module.exports = {
       company: 'Netcompany',
       position: 'Senior consultant',
       period: '2017 - Jul 2018',
-      badges: ['C#', '.Net', 'Ef6', 'REDIS', 'SOLR', 'Angular', 'Typescript'],
+      badges: ['C#', '.Net', 'EF6', 'REDIS', 'SOLR', 'Angular', 'Typescript'],
       contents: `DUBU 3.0 (Digitalisering - Udsatte Børn og Unge) project. Developing from scratch custom architecture following best practices such as SOLID, Onion Architecture and Inversion of Control. Introduction of OIOSAML secutiry layer into the solution using REDIS as cache. Integration of SOLR into the solution.`
     },
     {
       company: 'Software Engineer at GE Oil & Gas / Institute of Aviation',
       position: 'Software Engineer',
       period: '2014 - 2017',
-      badges: ['C#', '.Net', 'EF5', 'WPF', 'WCF', 'WindowsForms', "LISP", "TSQL"],
+      badges: ['C#', '.Net', 'EF6', 'WPF', 'WCF', 'WindowsForms', "LISP", "TSQL"],
       contents: `Development of software for CAD based systems using C# NET 4.0/4.5 and LISP.`
     },
     {
       company: 'Bravura Solutions',
       position: 'Application Engineer',
       period: '2012 - 2014',
-      badges: ['C#', '.Net', 'Java', "TSQL"],
+      badges: ['C#', '.Net', 'Java', 'Hibernate', "TSQL"],
       contents: `Maintance and developement of transfer agency platform.`
     }
   ]
